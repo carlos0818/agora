2fd3af2d10a2608bb38fddd9faf2f1c6122c2633
“Carlos <praimus666@gmail.com>
2023-05-04 16:14:22 -0500

    Remove background circles

--- components/Home/LogoutHome/LogoutHome.tsx
+++ components/Home/LogoutHome/LogoutHome.tsx
@@ -28,10 +28,10 @@ export const LogoutHome = () => {
     return (
         <>
             <section className={ home['rocket-main'] }>
-                <div className="circle-1"></div>
+                {/* <div className="circle-1"></div>
                 <div className="circle-2"></div>
                 <div className="circle-3"></div>
-                <div className="circle-4"></div>
+                <div className="circle-4"></div> */}
 
                 <div className={ home['rocket-container'] }>
                     <Image src={ rocket } alt='Rocket Image' className={ home['rocket-image'] } />
