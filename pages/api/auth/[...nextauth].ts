5c54f1df9238cebcaeaf95427d7be1d23d0cdcf9
“Carlos <praimus666@gmail.com>
2023-05-25 21:21:46 -0500

    Facebook login integration

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,6 +11,7 @@ declare module "next-auth" {
 }
 
 console.log(process.env.GOOGLE_CLIENT_ID)
+console.log(process.env.GOOGLE_CLIENT_SECRET)
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
