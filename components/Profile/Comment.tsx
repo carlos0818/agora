081a4f464058396a3a963f1979f4fd97ada8c474
“Carlos <praimus666@gmail.com>
2023-08-10 21:04:11 -0500

    User comments profile navigation

--- components/Profile/Comment.tsx
+++ components/Profile/Comment.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
 
 import styles from './comment.module.css'
@@ -11,16 +12,20 @@ interface Props {
 }
 
 export const Comment: FC<Props> = ({ comment }) => {
+    const router = useRouter()
+
     let date = comment.dateAdded
     if (comment.server === undefined) {
         date = convertTimeZone(comment.dateAdded!)
     }
 
-    // const date = convertTimeZone(comment.dateAdded)
+    const handleNavigateProfile = (userId: string) => {
+        router.push(`/profile/${ userId }`)
+    }
 
     return (
         <div className={ styles['comment-container'] }>
-            <div className={ styles['user-image'] }>
+            <div className={ styles['user-image'] } onClick={ () => handleNavigateProfile(comment.id!) }>
                 <Image
                     src={ comment.profilepic! }
                     alt=''
@@ -30,7 +35,7 @@ export const Comment: FC<Props> = ({ comment }) => {
                 />
             </div>
             <div className={ styles['info-container'] }>
-                <p className={ styles['user-name'] }>{ comment.fullname }</p>
+                <p className={ styles['user-name'] } onClick={ () => handleNavigateProfile(comment.id!) }>{ comment.fullname }</p>
                 <p className={ styles['date'] }>{ date }</p>
                 <p className={ styles['comment'] }>{ comment.body }</p>
             </div>
