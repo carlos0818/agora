3d448f10b63cfb194f0a29b76ee806b7603655b8
“Carlos <praimus666@gmail.com>
2023-09-15 16:32:03 -0500

    community slider

--- components/Home/LogoutHome/LogoutHome.tsx
+++ components/Home/LogoutHome/LogoutHome.tsx
@@ -90,6 +90,8 @@ export const LogoutHome = () => {
         const anchor = url.substring(url.indexOf("#")+1)
         const page = anchor.split('-')[1] ? Number(anchor.split('-')[1]) : 1
 
+        console.log(page)
+
         if(page > 1 && type === 'prev') {
             window.location.href = '#page-' + (page - 1)
         }
